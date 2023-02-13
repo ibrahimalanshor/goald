6ae184957e7dea08b0d3df7229df86b5bc43097e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-13 04:08:37 -0800

    feat: auth validation test

---
+++ test/auth/register/register.test.js
@@ -0,0 +1,24 @@
+const { describe, it, before, after } = require('mocha');
+const { expect } = require('chai');
+const supertest = require('supertest');
+const { createServer } = require('../../../lib/gwik/server');
+const config = require('../../../config/server.config');
+
+describe('resgister test', () => {
+  const server = createServer();
+  const path = '/register';
+
+  before(() => server.listen());
+  after(() => server.stop());
+
+  it('should return 422', async () => {
+    const res = await supertest(config.url).post(path).expect(422);
+
+    expect(res.body).to.have.property('errors');
+    expect(res.body.errors).to.have.property('email');
+    expect(res.body.errors).to.have.property('username');
+    expect(res.body.errors).to.have.property('name');
+    expect(res.body.errors).to.have.property('password');
+    expect(res.body.errors).to.have.property('password_confirmation');
+  });
+});
