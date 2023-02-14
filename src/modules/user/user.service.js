3ad8fb1aff0a6b26712d1968586c066113bd398e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-14 04:49:55 -0800

    feat: user service

---
+++ src/modules/user/user.service.js
@@ -0,0 +1,33 @@
+const { ConflictException } = require('gwik');
+const knex = require('../../../database/knex');
+
+async function createUser(user) {
+  try {
+    const [id] = await knex('users').insert({
+      email: user.email,
+      name: user.name,
+      username: user.username,
+      password: user.password,
+    });
+
+    return id;
+  } catch (err) {
+    if (err.errno === 1062) {
+      throw new ConflictException();
+    }
+
+    throw err;
+  }
+}
+
+async function findUserId(id) {
+  return await knex('users').where('id', id).first();
+}
+
+async function deleteUserId(id) {
+  return await knex('users').where('id', id).delete();
+}
+
+exports.createUser = createUser;
+exports.findUserId = findUserId;
+exports.deleteUserId = deleteUserId;
